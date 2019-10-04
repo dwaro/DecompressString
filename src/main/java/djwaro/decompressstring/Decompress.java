b520583c85754f108955799b30c5a31c84a7d5c2
dwaro <dwaro@wisc.edu>
2019-10-04 12:53:43 -0500

    Logic mostly there, need to fine tune end and begging case

--- src/main/java/djwaro/decompressstring/Decompress.java
+++ src/main/java/djwaro/decompressstring/Decompress.java
@@ -8,86 +8,106 @@ public class Decompress {
     private String nums = "123456789";
     private String letters = "abcdefghijklmnopqrstuvwxyz";
     private ArrayList<String[]> stack = new ArrayList<String[]>();
-    private ArrayList<Integer> depthList = new ArrayList<Integer>();
-    private ArrayList<Integer> numList = new ArrayList<Integer>();
-    private ArrayList<String> stringList = new ArrayList<String>();
-    private String newString = "";
+    private String newString;
+    private String tempStr;
+    private String num;
+    private int level;
     
     Decompress() {
+        newString = "";
+        tempStr = "";
+        num = "1";
+        level = 1;
     }
     
     public String decomp(String str) {
-        int[] bracketCounts = {0, 0};
+        newString = "";
+        tempStr = "";
+        num = "1";
+        level = 1;
+        stack.clear();
+        
         String[] split = str.split("");
-        tryThree(bracketCounts, split, 0);
+        iterateString(split);
         
         return newString;
     }
     
-    void tryThree(int[] bracketCounts, String[] split, int index) {
-        System.out.println(bracketCounts[0]);
-        System.out.println(bracketCounts[1]);
+    void iterateString(String[] split) {
+        String grow_str = "";
+        String grow_num = "";
         
-        int lCount = bracketCounts[0];
-        int rCount = bracketCounts[1];
-        
-        // remove items from stack once we reach closure point
-        if (lCount == rCount & stack.size() > 1) {
-            String tempStr;
-            String startStr = stack.get(stack.size()-1)[1]; // str of last item
-            while (stack.size() > 1) {
-                
-                tempStr = "";
-                int num = Integer.parseInt(stack.get(stack.size()-1)[0]); // num of last item
-                String str = stack.get(stack.size()-2)[1]; // str of second to last item
-                
-                for (int i=0; i<num; i++) {
-                    tempStr+=startStr;
+        for (int i = 0; i < split.length; i++) {
+            grow_str = "";
+            grow_num = "";
+            
+            System.out.println("newString: " + newString);
+            
+            try {
+                while (letters.contains(split[i])) {
+                    grow_str += split[i];
+                    i++;
                 }
-                str += tempStr;
-                startStr = str;
-                stack.remove(stack.size()-1); // remove last item from stack
+            } catch (ArrayIndexOutOfBoundsException e) {}
+            
+            if (!grow_str.equals("") & num != "") {
+                String[] sequence = {num, grow_str};
+                stack.add(sequence);
             }
-            stack.remove(stack.size()-1);
-            newString += startStr;
-        } else if (lCount == rCount & stack.size() == 1) {
-            newString += stack.get(0)[1];
-        }
-        
-        if (index < split.length) {
-            String num = "";
+            
+            num = "";
+            
             try {
-                while (nums.contains(split[index])) {
-                    num += split[index];
-                    index++;
+                while (nums.contains(split[i])) {
+                    grow_num += split[i];
+                    i++;
                 }
             } catch (ArrayIndexOutOfBoundsException e) {}
-
-            try {
-                while (split[index].equals("[") | split[index].equals("]")) {
-                    if (split[index].equals("[")){
-                        bracketCounts[0] += 1;
-                    } else {
-                        bracketCounts[1] += 1;
-                    }
-                    index++;
+            
+            num = grow_num;
+            
+            if  (i < split.length) {
+                if (split[i].equals("[")) {
+                    level++;
+                } else if (split[i].equals("]")) {
+                    level--;
+                    popstack();
                 }
-            } catch (ArrayIndexOutOfBoundsException e) {}
-
-            String holder = "";
-            try {
-                while (letters.contains(split[index])){
-                    holder += split[index];
-                        index++;
-                    }
-            } catch (ArrayIndexOutOfBoundsException e) {}
-           
-
-            if (!holder.equals("")) {
-                String[] adder = {num, holder};
-                stack.add(adder);
-                tryThree(bracketCounts, split, index);
-            }   
+            }
         }
     }
+    
+    void popstack() {
+        int count = 1;
+        String lastStr = "";
+        String thisStr = "";
+        String temp2 = "";
+        
+        if (stack.size() > 2) {
+            String[] c = stack.get(stack.size()-1);
+            count = Integer.parseInt(c[0]);
+            lastStr = c[1];
+            for (int j = 0; j < count; j++) {
+                tempStr += lastStr;
+            }
+            stack.remove(stack.size()-1);
+        } else if (stack.size() == 2) {
+            while (stack.size() > 0) {
+                thisStr = stack.get(stack.size()-1)[1];
+                thisStr += tempStr;
+                temp2 = thisStr;
+                if (stack.size() == 2) {
+                    for (int j = 0; j < Integer.parseInt(stack.get(stack.size()-1)[0]) - 1; j++) {
+                        thisStr += temp2;
+                    }
+                    tempStr = thisStr;
+                } else {
+                    this.newString += thisStr;
+                    tempStr = "";
+                    num = "1";
+                }
+                stack.remove(stack.size()-1);
+            }
+        }
+    } 
 }
--- src/main/java/djwaro/decompressstring/Main.java
+++ src/main/java/djwaro/decompressstring/Main.java
@@ -5,9 +5,14 @@ package djwaro.decompressstring;
  */
 public class Main {
     public static void main(String[] args) {
-        Decompress decomp = new Decompress();
+        Decompress decomper = new Decompress();
         //Decompress decomp = new Decompress("3[abc]4[ab]c");
-        String newString = decomp.decomp("a3[ab2[m]]a2[b]f");
+        String newString = decomper.decomp("a3[ab2[m]]a2[b]f");
+        String newString2 = decomper.decomp("2[t]3[a2[b]]");
+        System.out.println("--------------");
         System.out.println(newString);
+        System.out.println(newString2);
+        System.out.println("--------------");
+        
     }
 }
