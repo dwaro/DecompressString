77eea7f2af3c4196c7960b8f8c32662ea3c89e17
dwaro <dwaro@wisc.edu>
2019-10-03 21:33:42 -0500

    working on recursive

--- src/main/java/djwaro/decompressstring/Decompress.java
+++ src/main/java/djwaro/decompressstring/Decompress.java
@@ -5,43 +5,89 @@ import java.util.*;
  * @author David J. Waro
  */
 public class Decompress {
-    private String brackets = "[]";
     private String nums = "123456789";
     private String letters = "abcdefghijklmnopqrstuvwxyz";
-    private int index = 0;
+    private ArrayList<String[]> stack = new ArrayList<String[]>();
+    private ArrayList<Integer> depthList = new ArrayList<Integer>();
+    private ArrayList<Integer> numList = new ArrayList<Integer>();
+    private ArrayList<String> stringList = new ArrayList<String>();
+    private String newString = "";
     
     Decompress() {
     }
     
     public String decomp(String str) {
-        // construct array list from string to iterate
-        String split[] = str.split("");
-        index = 0;
-        String newString = "";
-        String num = "";
-        newString = recurseString(split, newString, index, num);
+        int[] bracketCounts = {0, 0};
+        String[] split = str.split("");
+        tryThree(bracketCounts, split, 0);
         
         return newString;
     }
     
-    public String recurseString(String[] split, String newString, int index, String num){
+    void tryThree(int[] bracketCounts, String[] split, int index) {
+        System.out.println(bracketCounts[0]);
+        System.out.println(bracketCounts[1]);
         
-        // letters at level 0
-        while (letters.contains(split[index])) {
-            newString += split[index];
-            index++;
+        int lCount = bracketCounts[0];
+        int rCount = bracketCounts[1];
+        
+        // remove items from stack once we reach closure point
+        if (lCount == rCount & stack.size() > 1) {
+            String tempStr;
+            String startStr = stack.get(stack.size()-1)[1]; // str of last item
+            while (stack.size() > 1) {
+                
+                tempStr = "";
+                int num = Integer.parseInt(stack.get(stack.size()-1)[0]); // num of last item
+                String str = stack.get(stack.size()-2)[1]; // str of second to last item
+                
+                for (int i=0; i<num; i++) {
+                    tempStr+=startStr;
+                }
+                str += tempStr;
+                startStr = str;
+                stack.remove(stack.size()-1); // remove last item from stack
+            }
+            stack.remove(stack.size()-1);
+            newString += startStr;
+        } else if (lCount == rCount & stack.size() == 1) {
+            newString += stack.get(0)[1];
         }
         
-        // number at level 0
-        while (nums.contains(split[index])) {
-            num += split[index];
-            index++;
+        if (index < split.length) {
+            String num = "";
+            try {
+                while (nums.contains(split[index])) {
+                    num += split[index];
+                    index++;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {}
+
+            try {
+                while (split[index].equals("[") | split[index].equals("]")) {
+                    if (split[index].equals("[")){
+                        bracketCounts[0] += 1;
+                    } else {
+                        bracketCounts[1] += 1;
+                    }
+                    index++;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {}
+
+            String holder = "";
+            try {
+                while (letters.contains(split[index])){
+                    holder += split[index];
+                        index++;
+                    }
+            } catch (ArrayIndexOutOfBoundsException e) {}
+           
+
+            if (!holder.equals("")) {
+                String[] adder = {num, holder};
+                stack.add(adder);
+                tryThree(bracketCounts, split, index);
+            }   
         }
-        
-        if (split[index].equals("]")) {
-            
-        }
-        
-        return "asdfsa";
     }
 }
--- src/main/java/djwaro/decompressstring/Main.java
+++ src/main/java/djwaro/decompressstring/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Decompress decomp = new Decompress();
         //Decompress decomp = new Decompress("3[abc]4[ab]c");
-        String newString = decomp.decomp("3[abc]4[ab]c");
+        String newString = decomp.decomp("a3[ab2[m]]a2[b]f");
         System.out.println(newString);
     }
 }
